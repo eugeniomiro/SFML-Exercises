aef2b760ec126bae74c66ae5769b31c1f76163ff
Eugenio Miró <eugenio.miro@gmail.com>
2020-11-23 08:20:07 -0300

    restart tutorial from cmake repo

--- CMakeTutorial/Step1/tutorial.cxx
+++ CMakeTutorial/Step1/tutorial.cxx
@@ -3,15 +3,10 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
-#include "TutorialConfig.h"
-#ifdef USE_MYMATH
-#  include "MathFunctions.h"
-#endif
+
 int main(int argc, char* argv[])
 {
   if (argc < 2) {
-    std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
-              << Tutorial_VERSION_MINOR << std::endl;
     std::cout << "Usage: " << argv[0] << " number" << std::endl;
     return 1;
   }
@@ -20,11 +15,7 @@ int main(int argc, char* argv[])
   const double inputValue = atof(argv[1]);
 
   // calculate square root
-#ifdef USE_MYMATH
-  const double outputValue = mysqrt(inputValue);
-#else
   const double outputValue = sqrt(inputValue);
-#endif
   std::cout << "The square root of " << inputValue << " is " << outputValue
             << std::endl;
   return 0;
