755b3953a15264f2b439fbc8b31ea189782088f0
Eugenio Miró <eugenio.miro@gmail.com>
2020-12-09 19:06:53 -0300

    use cmake to configure version

--- cfirst/cfirst.cpp
+++ cfirst/cfirst.cpp
@@ -1,4 +1,9 @@
+#include <iostream>
+#include "config.h"
+
 int main(int argc, char** argv)
 {
+	std::cout << "cfirst version " << cfirst_VERSION_MAJOR <<
+		                   "." << cfirst_VERSION_MINOR << std::endl;
 	return 0;
 }
